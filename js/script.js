6f3176b164cfe01b3227331a0c31680837d33c81
Tetyana <tsergata@gmail.com>
2020-01-09 09:20:31 +0100

    work on position

--- js/script.js
+++ js/script.js
@@ -14,19 +14,24 @@ console.log(listUsers);
 
 var newUser = prompt('Inserisci il tuo cognome');
 console.log(newUser);
+// listUsers.sort();
 
 // 2.Inserire il cognome in un array
 listUsers.push(newUser);
-// listUsers.sort();
-
+listUsers.sort()
 
 // 3.Ordinare alfabeticamente e stampare
 for (var i = 0; i < listUsers.length; i++){
-  console.log(listUsers[i]);
+  // console.log(listUsers[i]);
   var listContent = document.getElementById('surname_list').innerHTML;
   document.getElementById('surname_list').innerHTML = listContent + '<li>' + listUsers[i] + '</li>';
   console.log(listContent);
+  if (newUser == listUsers[i]) {
+    console.log(listUsers.indexOf(newUser));
+  }
 }
-listUsers.sort();
+// listUsers.sort();
 
 // 4.La nuova posizione nella lista
+
+console.log(listUsers.indexOf(newUser));
